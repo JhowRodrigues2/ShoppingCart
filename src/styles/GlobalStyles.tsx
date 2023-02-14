30904decb0bcac8a5b3f4e1baec66a3c2ef7163a
Jhonatta Rodrigues <107954865+JhowRodrigues2@users.noreply.github.com>
2023-02-14 20:36:56 -0300

    initial config

---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Lato', sans-serif;  }
+html,body{
+  height: 100%;
+}
+`;
+
+export default GlobalStyle;
